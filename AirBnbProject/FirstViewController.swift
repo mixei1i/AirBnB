290534ecf330a8d626fb060016299e8c8ed355d0
mikheil <mikheil.pukhashvili11@geolab.edu.ge>
2020-09-28 21:17:50 +0400

    Initial commit

---
+++ AirBnbProject/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AirBnbProject
+//
+//  Created by Admin on 9/26/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AirBnbProject/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AirBnbProject
+//
+//  Created by Admin on 9/26/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
